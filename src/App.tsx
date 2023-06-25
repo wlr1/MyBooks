3adedc7e897636be53097967f99447e51861980f
wlr1 <uns4d123@gmail.com>
2023-06-25 17:05:42 +0300

    adding top scroll/adding some css styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,26 @@ import Header from "./Components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 const App: React.FC = () => {
+  const [showIcon, setShowIcon] = React.useState<boolean>(false);
+
+  const handleScroll = () => {
+    setShowIcon(window.scrollY >= 600);
+  };
+
+  const setCoords = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
     <div>
       <div>
@@ -13,9 +33,28 @@ const App: React.FC = () => {
           <Outlet />
         </div>
       </div>
+      {showIcon && (
+        <div className="sticky bottom-0 right-0">
+          <div className="flex justify-end mr-24">
+            <a onClick={setCoords} className="mt-56 cursor-pointer">
+              <svg
+                className="w-20 "
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M12 2a10 10 0 0 1 2 19.8V12h3l-5-5-5 5h3v9.8A10 10 0 0 1 12 2z"
+                  data-name="Circle atas"
+                  fill="#4c394e"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-
+// window.scrollTo(0, 0)
 export default App;
--- src/Components/Books/Books.tsx
+++ src/Components/Books/Books.tsx
@@ -8,23 +8,23 @@ const Books: React.FC = () => {
   const loading = useSelector((state: RootState) => state.book.loading);
 
   return (
-    <div className="grid grid-cols-3 grid-rows-3 gap-6 px-36 py-20">
+    <div className="grid grid-cols-3 grid-rows-3 gap-16 px-36 py-20">
       {loading ? (
         <div>Loading...</div>
       ) : error ? (
         <div>Error: {error}</div>
       ) : (
         books.map((book) => (
-          <div key={book.id} className="w-56">
+          <div key={book.id} className="w-64 h-96  shadow-2xl">
             <img
-              className="w-56 h-72 mb-2"
+              className="w-64 h-72 mb-2"
               src={book.volumeInfo.imageLinks?.thumbnail}
               alt="books"
             />
-            //if length is higher 25 then display ... otherwise title
-            <h4 className="font-bold ">
-              {book.volumeInfo.title.length > 25
-                ? `${book.volumeInfo.title.slice(0, 25)}...`
+
+            <h4 className="font-bold mt-4 w-64 text-center">
+              {book.volumeInfo.title.length > 21
+                ? `${book.volumeInfo.title.slice(0, 21)}...`
                 : book.volumeInfo.title}
             </h4>
           </div>
